14622add3c49abff773b44c19066b6df7d15b01f
matheuseuseus <matheus.kder@gmail.com>
2018-08-14 21:28:11 -0300

    Small correction when showing items

--- src/components/App.js
+++ src/components/App.js
@@ -24,22 +24,24 @@ class App extends Component {
     return(
       <div className="App">
         <h1>Alerts</h1>
-          {
-            alerts.map(alert => 
-              <div class="card" key={alert._id}>
-                <h2>{alert.MissionInfo.location}</h2>
-                <h3>{alert.MissionInfo.missionType}</h3>
-                Rewards<br />
-                Credits: {alert.MissionInfo.missionReward.credits}<br />
-                { alert.MissionInfo.missionReward.items ?
-                    alert.MissionInfo.missionReward.items.map(item => {
-                      Item: <img src={item} alt="Item" />
-                    })
-                  :
-                    <span>No items</span>
-                }
-              </div>
-            )
+          { !alerts ?
+              <h1>No alerts available.</h1>
+            :
+              alerts.map(alert => 
+                <div className="card" key={alert._id}>
+                  <h2>{alert.MissionInfo.location}</h2>
+                  <h3>{alert.MissionInfo.missionType}</h3>
+                  Rewards<br />
+                  Credits: {alert.MissionInfo.missionReward.credits}<br />
+                  { alert.MissionInfo.missionReward.items ?
+                      alert.MissionInfo.missionReward.items.map(item => {
+                        return <span>Item: {item.substring(item.lastIndexOf("/") + 1)}</span>
+                      })
+                    :
+                      <span>No items</span>
+                  }
+                </div>
+              )
           }
       </div>
     )
